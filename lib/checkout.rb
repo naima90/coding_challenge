55fb1e658cd9c62c194998517d0d55873b4b698b
Naima <naimaosman203@gmail.com>
2022-06-17 13:10:40 +0100

    checks if item exists before scanning

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,15 +1,24 @@
 class Checkout
 
   def initialize
+    @products = [ {code: 001, name: "Lavender heart", price: 9.25}, {code: 002, name: "Personalised cufflinks", price: 45.00}, {code: 003, name: "Kids T-shirt", price: 19.95} ]
     @basket = []
   end
 
   def scan(item)
-    @basket << item
-    @basket.join(" ") 
+    product_name = []
+    @products.map do |product|
+      product_name << product[:name].downcase
+    end
+    if product_name.include?(item.downcase)
+      @basket << item
+      return item
+    else 
+      "Product's not available"
+    end
   end
 
-  def price
+  def total_price
     lavender = []
     @basket.map do |item| 
       if item == "Lavender heart"
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -6,12 +6,16 @@ describe Checkout do
 
   it "should be able to return the price of Lavender heart" do
     subject.scan("Lavender heart")
-    expect(subject.price).to eq(9.25)
+    expect(subject.total_price).to eq(9.25)
   end
 
   it "should be able to return the price of 2 Lavender heart" do
     subject.scan("Lavender heart")
     subject.scan("Lavender heart")
-    expect(subject.price).to eq(17.00)
+    expect(subject.total_price).to eq(17.00)
+  end
+
+  it "shouldn't be able to scan products that aren't available" do
+    expect(subject.scan("Apple")).to eq("Product's not available")
   end
 end
