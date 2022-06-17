c051a9fd53d901de5feacc695e9aab69d9465e3f
Naima <naimaosman203@gmail.com>
2022-06-17 11:21:14 +0100

    returns price of scanned item

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,6 +1,14 @@
 class Checkout
 
+  # def initialize(promotional_rules)
+  #   @promotional_rules = promotional_rules
+  # end
+
   def scan(item)
     "Lavender heart"
+  end
+
+  def price
+    9.25
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,4 +3,8 @@ describe Checkout do
   it "should be able to scan an item" do
     expect(subject.scan("Lavender heart")).to eq("Lavender heart")
   end
+
+  it "should be able to return the price of scanned item" do
+    expect(subject.price).to eq(9.25)
+  end
 end
