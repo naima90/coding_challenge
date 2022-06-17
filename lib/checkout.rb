653dca0e94baf6c8b975861309fd225001c9eabf
Naima <naimaosman203@gmail.com>
2022-06-17 17:20:32 +0100

    added linter

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,13 @@
-class Checkout
+# frozen_string_literal: true
 
+# this is checkout class
+class Checkout
   def initialize
-    @products = [ {code: 001, name: "Lavender heart", price: 9.25}, {code: 002, name: "Personalised cufflinks", price: 45.00}, {code: 003, name: "Kids T-shirt", price: 19.95} ]
+    @products = [
+      { code: 0o01, name: 'Lavender heart', price: 9.25 },
+      { code: 0o02, name: 'Personalised cufflinks', price: 45.00 },
+      { code: 0o03, name: 'Kids T-shirt', price: 19.95 }
+    ]
     @basket = []
   end
 
@@ -12,8 +18,8 @@ class Checkout
     end
     if product_name.include?(item.downcase)
       @basket << item
-      return item
-    else 
+      item
+    else
       "Product's not available"
     end
   end
@@ -22,18 +28,12 @@ class Checkout
     price = 0
     lavender = []
     @basket.map do |item|
-      lavender << item if item == "Lavender heart"
+      lavender << item if item == 'Lavender heart'
       @products.map do |product|
-        if product[:name].include?(item) 
-          price += product[:price]
-        end
+        price += product[:price] if product[:name].include?(item)
       end
     end
-    if lavender.length >= 2
-      price -= (lavender.length * 0.75)
-    end
+    price -= (lavender.length * 0.75) if lavender.length >= 2
     price > 60.00 ? price -= (price * 10 / 100) : price
   end
 end
-
-  
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 require 'checkout'
 describe Checkout do
-  it "should be able to scan an item" do
-    expect(subject.scan("Lavender heart")).to eq("Lavender heart")
+  it 'should be able to scan an item' do
+    expect(subject.scan('Lavender heart')).to eq('Lavender heart')
   end
 
-  it "should be able to return the price of Lavender heart" do
-    subject.scan("Lavender heart")
+  it 'should be able to return the price of Lavender heart' do
+    subject.scan('Lavender heart')
     expect(subject.total_price).to eq(9.25)
   end
 
-  it "should be able to return the price of 2 Lavender heart" do
-    subject.scan("Lavender heart")
-    subject.scan("Lavender heart")
+  it 'should be able to return the price of 2 Lavender heart' do
+    subject.scan('Lavender heart')
+    subject.scan('Lavender heart')
     expect(subject.total_price).to eq(17.00)
   end
 
-  it "should be able to return the price of 2 different items" do
-    subject.scan("Lavender heart")
-    subject.scan("Personalised cufflinks")
+  it 'should be able to return the price of 2 different items' do
+    subject.scan('Lavender heart')
+    subject.scan('Personalised cufflinks')
     expect(subject.total_price).to eq(54.25)
   end
 
-   it "should be able to return the price of 3 different items" do
-    subject.scan("Lavender heart")
-    subject.scan("Personalised cufflinks")
-    subject.scan("Kids T-shirt")
+  it 'should be able to return the price of 3 different items' do
+    subject.scan('Lavender heart')
+    subject.scan('Personalised cufflinks')
+    subject.scan('Kids T-shirt')
     expect(subject.total_price).to eq(66.78)
   end
 
   it "shouldn't be able to scan products that aren't available" do
-    expect(subject.scan("Apple")).to eq("Product's not available")
+    expect(subject.scan('Apple')).to eq("Product's not available")
   end
 end
